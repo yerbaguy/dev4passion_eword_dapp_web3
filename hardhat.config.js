988ff7169342114903c3de6434823962a805e199
System Administrator <root@bartoszs-MacBook-Pro.local>
2023-01-21 11:42:44 +0100

    dev4passion_eword_web3_backup_26_10_2022_shows_random_plword

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    //},
+    },
+  }
+};
