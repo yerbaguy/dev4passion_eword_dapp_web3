4108c566f15be169391d14a1c11e5043a0f6a097
System Administrator <root@bartoszs-MacBook-Pro.local>
2023-02-07 00:17:40 +0100

    Added to my-react-dapp

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,22 @@
-require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-waffle");
-require('dotenv').config;
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
   networks: {
-    hardhat: {},
-    goerli: {
-      // url: process.url.API_URL,
-      // accounts: [process.env.PRIVATE_KEY],
-      // https://eth-goerli.g.alchemy.com/v2/1NkuHJk9fySa1xwgPZ21rwqkGJbh_9Cm
-      // url: "https://goerli.infura.io/v3/",
-      url: "https://eth-goerli.g.alchemy.com/v2/1NkuHJk9fySa1xwgPZ21rwqkGJbh_9Cm",
-      // d6a736bafc7f7a6ec508475555533eae388590c4d16748afee99f615ff7908dd
-      // accounts: [`0xd6a736bafc7f7a6ec508475555533eae388590c4d16748afee99f615ff7908dd`],
-      accounts: [`0xd6a736bafc7f7a6ec508475555533eae388590c4d16748afee99f615ff7908dd`],
-      // accounts: ['d6a736bafc7f7a6ec508475555533eae388590c4d16748afee99f615ff7908dd'],
-  
+    hardhat: {
+      chainId: 1337
     },
-
-
-
-  }
-  // goerli: {
-  //   // url: process.url.API_URL,
-  //   // accounts: [process.env.PRIVATE_KEY],
-  //   url: "https://goerli.infura.io/v3/",
-  //   accounts: ['d6a736bafc7f7a6ec508475555533eae388590c4d16748afee99f615ff7908dd'],
-
-  // },
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.17",
 };
