8e25221c18b3ddc3242a81999a50a5dcbdfc844f
System Administrator <root@bartoszs-MacBook-Pro.local>
2023-02-06 14:10:02 +0100

    Added to dev4passion_ipfs

---
+++ app.js
@@ -0,0 +1,26 @@
+const { create } = require("ipfs-http-client");
+const fs = requre("fs");
+
+async function ipfsClient() {
+
+    const ipfs = await create (
+
+        {
+            host: "ipfs.infura.io",
+            port: 5001,
+            protocol: "https"
+        }
+    );
+
+    return ipfs;
+}
+
+async function saveText() {
+
+    let ipfs = await ipfsClient();
+
+    let result = await ipfs.add("hello there");
+    console.log(result);
+}
+
+saveText()
