9d108cbe56fb407ec6cc7b65a11b4eee791c7a78
System Administrator <root@bartoszs-MacBook-Pro.local>
2023-02-06 13:12:04 +0100

    Added to dev4passion_eword_web3_backup_30_11_2022_ipfs-hash

---
+++ config.js
@@ -0,0 +1,7 @@
+
+  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  export const ewordTokencontractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  export const ewordTokenownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+
+  
